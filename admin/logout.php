bdebfc3c1725e247f87c44cde4108cfc4158ad6f
Muhammad MF <63154480+muhmahezaf@users.noreply.github.com>
2023-07-16 09:15:49 +0700

    Add files via upload

---
+++ admin/logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+unset($_SESSION['idinv']);
+unset($_SESSION['userinv']);
+unset($_SESSION['passinv']);
+unset($_SESSION['namainv']);
+unset($_SESSION['teleponinv']);
+unset($_SESSION['fotoinv']);
+echo "<script>window.location='../'</script>";
+//session_destroy();
+?>
